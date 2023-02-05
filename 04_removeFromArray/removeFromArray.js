3f1b3b44553a49ea15df3b4d2f01f99241c7e59a
pdostonh <pdostonh@gmail.com>
2023-02-05 20:12:45 +0500

    solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,8 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(theArray, ...elems) {
+    for (let i = 0; i < elems.length; i++) {
+        theArray = theArray.filter(els => els !== elems[i]);
+    }
+    return theArray;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,12 @@
-const sumAll = function() {
-
+const sumAll = function(...lims) {
+    let sum = 0;
+    let max = lims[0] >= lims[1] ? lims[0] : lims[1];
+    let min = lims[0] >= lims[1] ? lims[1] : lims[0];
+    if (max < 0 || min < 0 || typeof(max) !== 'number' || typeof(min) !== 'number') return "ERROR";
+    for(let i = min; i <= max; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
